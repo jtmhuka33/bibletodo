e687812d281e9de3a0a679fc1bf4aa1acce0941c
John.Mhuka <jtmhuka33@gmail.com>
2025-05-15 19:14:16 +0200

    Initial Commit

---
+++ bibletodoTests/bibletodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  bibletodoTests.swift
+//  bibletodoTests
+//
+//  Created by John on 15.05.25.
+//
+
+import Testing
+@testable import bibletodo
+
+struct bibletodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ bibletodoUITests/bibletodoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  bibletodoUITestsLaunchTests.swift
+//  bibletodoUITests
+//
+//  Created by John on 15.05.25.
+//
+
+import XCTest
+
+final class bibletodoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
